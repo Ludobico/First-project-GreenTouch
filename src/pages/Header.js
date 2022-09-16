64d406356d1f973450936757ac394da5a4b312ab
김찬진 <aqs450@gmail.com>
2022-09-16 09:44:21 +0900

    login

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -75,7 +75,7 @@ function Header() {
         </button>
         {Login && (
           <button className="loginBtn" onClick={GotoLoginForm} ref={loginRef}>
-            로그인1
+            로그인
           </button>
         )}
         {LogOut && (
